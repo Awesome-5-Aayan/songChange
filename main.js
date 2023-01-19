f7b8d4c39347902d36d28757e9e1f5b4ed2e099c
Trixlr <81710455+Awesome-5-Aayan@users.noreply.github.com>
2023-01-19 16:25:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+RightWristY=0;
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -11,9 +15,32 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
-
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded(){
+    console.log('posenet is initialized');
+    
+}
+function gotPoses(results){
+if (results.length>0) {
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("left wrist X= "+ leftWristX + " Left Wrist Y= "+ leftWristY )
+    console.log("right wrist X= "+ rightWristX + " Right Wrist Y= "+ rightWristY )
+}
 }
